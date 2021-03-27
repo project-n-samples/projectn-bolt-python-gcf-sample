3a03feb6f0d35e0dca3bc5dbd5f5a42769113568
Amit Warke <aw@projectn.co>
2021-03-26 18:18:49 -0700

    fixes

--- BoltGSPerf.py
+++ BoltGSPerf.py
@@ -2,6 +2,7 @@ import os
 import time
 import random
 import string
+import json
 import math
 from statistics import mean
 from statistics import median_low
@@ -87,11 +88,11 @@ class BoltGSPerf:
                     self._request_type == "DOWNLOAD_OBJECT_PASSTHROUGH_TTFB":
                 return self._download_object_passthrough_perf(bucket_name)
             elif self._request_type == "UPLOAD_OBJECT":
-                self._upload_object_perf(bucket_name)
+                return self._upload_object_perf(bucket_name)
             elif self._request_type == "DELETE_OBJECT":
-                self._delete_object_perf(bucket_name)
+                return self._delete_object_perf(bucket_name)
             elif self._request_type == "ALL":
-                self._all_perf(bucket_name)
+                return self._all_perf(bucket_name)
         except Exception as e:
             return {
                 'errorMessage': str(e),
@@ -132,10 +133,14 @@ class BoltGSPerf:
         # calc bolt perf stats.
         bolt_list_objects_perf_stats = self._compute_perf_stats(self._bolt_op_times, self._bolt_op_tp)
 
-        return {
+        list_objects_perf_stats = {
             'gs_list_objs_perf_stats': gs_list_objects_perf_stats,
             'bolt_list_objs_perf_stats': bolt_list_objects_perf_stats
         }
+        if self._request_type == "ALL":
+            return list_objects_perf_stats
+        else:
+            return json.dumps(list_objects_perf_stats, indent=4, sort_keys=True)
 
     def _download_object_perf(self, bucket_name):
         # Get blobs from Bolt/GS.
@@ -143,46 +148,48 @@ class BoltGSPerf:
             # Get blob from GS.
             bucket = self._gs_storage_client.bucket(bucket_name)
             blob = bucket.get_blob(key)
-            obj_download_start_time = time.time()
-            if self._request_type == "DOWNLOAD_OBJECT_TTFB":
-                # get first byte of object
-                blob.download_as_bytes(start=0, end=0)
-            else:
-                # read the entire object.
-                blob.download_as_bytes()
-            obj_download_end_time = time.time()
-            # calc latency
-            download_obj_time = obj_download_end_time - obj_download_start_time
-            self._gs_op_times.append(download_obj_time)
-            # count object
-            if blob.content_encoding == "gzip" or str(key).endswith('.gz'):
-                self._gs_cmp_obj_count += 1
-            else:
-                self._gs_uncmp_obj_count += 1
-            # get blob size
-            self._gs_obj_sizes.append(blob.size)
+            if blob.size > 0:
+                obj_download_start_time = time.time()
+                if self._request_type == "DOWNLOAD_OBJECT_TTFB":
+                    # get first byte of object
+                    blob.download_as_bytes(start=0, end=0)
+                else:
+                    # read the entire object.
+                    blob.download_as_bytes()
+                obj_download_end_time = time.time()
+                # calc latency
+                download_obj_time = obj_download_end_time - obj_download_start_time
+                self._gs_op_times.append(download_obj_time)
+                # count object
+                if blob.content_encoding == "gzip" or str(key).endswith('.gz'):
+                    self._gs_cmp_obj_count += 1
+                else:
+                    self._gs_uncmp_obj_count += 1
+                # get blob size
+                self._gs_obj_sizes.append(blob.size)
 
             # Get blob from Bolt.
             bucket = self._bolt_storage_client.bucket(bucket_name)
             blob = bucket.get_blob(key)
-            obj_download_start_time = time.time()
-            if self._request_type == "DOWNLOAD_OBJECT_TTFB":
-                # get first byte of object
-                blob.download_as_bytes(start=0, end=0)
-            else:
-                # read the entire object.
-                blob.download_as_bytes()
-            obj_download_end_time = time.time()
-            # calc latency
-            download_obj_time = obj_download_end_time - obj_download_start_time
-            self._bolt_op_times.append(download_obj_time)
-            # count object
-            if blob.content_encoding == "gzip" or str(key).endswith('.gz'):
-                self._bolt_cmp_obj_count += 1
-            else:
-                self._bolt_uncmp_obj_count += 1
-            # get blob size
-            self._bolt_obj_sizes.append(blob.size)
+            if blob.size > 0:
+                obj_download_start_time = time.time()
+                if self._request_type == "DOWNLOAD_OBJECT_TTFB":
+                    # get first byte of object
+                    blob.download_as_bytes(start=0, end=0)
+                else:
+                    # read the entire object.
+                    blob.download_as_bytes()
+                obj_download_end_time = time.time()
+                # calc latency
+                download_obj_time = obj_download_end_time - obj_download_start_time
+                self._bolt_op_times.append(download_obj_time)
+                # count object
+                if blob.content_encoding == "gzip" or str(key).endswith('.gz'):
+                    self._bolt_cmp_obj_count += 1
+                else:
+                    self._bolt_uncmp_obj_count += 1
+                # get blob size
+                self._bolt_obj_sizes.append(blob.size)
 
         # calc gs perf stats
         gs_download_obj_perf_stats = self._compute_perf_stats(self._gs_op_times, obj_sizes=self._gs_obj_sizes)
@@ -198,7 +205,7 @@ class BoltGSPerf:
             gs_dwnld_obj_stat_name = 'gs_download_obj_perf_stats'
             bolt_dwnld_obj_stat_name = 'bolt_download_obj_perf_stats'
 
-        return {
+        download_obj_perf_stats = {
             gs_dwnld_obj_stat_name: gs_download_obj_perf_stats,
             'gs_object_count (compressed)': self._gs_cmp_obj_count,
             'gs_object_count (uncompressed)': self._gs_uncmp_obj_count,
@@ -206,6 +213,10 @@ class BoltGSPerf:
             'bolt_object_count (compressed)': self._bolt_cmp_obj_count,
             'bolt_object_count (uncompressed)': self._bolt_uncmp_obj_count,
         }
+        if self._request_type == "ALL":
+            return download_obj_perf_stats
+        else:
+            return json.dumps(download_obj_perf_stats, indent=4, sort_keys=True)
 
     def _download_object_passthrough_perf(self, bucket_name):
         # Get Objects via passthrough from Bolt.
@@ -213,24 +224,25 @@ class BoltGSPerf:
             # Get blob from Bolt.
             bucket = self._bolt_storage_client.bucket(bucket_name)
             blob = bucket.get_blob(key)
-            obj_download_start_time = time.time()
-            if self._request_type == "DOWNLOAD_OBJECT_PASSTHROUGH_TTFB":
-                # get first byte of object
-                blob.download_as_bytes(start=0, end=0)
-            else:
-                # read the entire object.
-                blob.download_as_bytes()
-            obj_download_end_time = time.time()
-            # calc latency
-            download_obj_time = obj_download_end_time - obj_download_start_time
-            self._bolt_op_times.append(download_obj_time)
-            # count object
-            if blob.content_encoding == "gzip" or str(key).endswith('.gz'):
-                self._bolt_cmp_obj_count += 1
-            else:
-                self._bolt_uncmp_obj_count += 1
-            # get blob size
-            self._bolt_obj_sizes.append(blob.size)
+            if blob.size > 0:
+                obj_download_start_time = time.time()
+                if self._request_type == "DOWNLOAD_OBJECT_PASSTHROUGH_TTFB":
+                    # get first byte of object
+                    blob.download_as_bytes(start=0, end=0)
+                else:
+                    # read the entire object.
+                    blob.download_as_bytes()
+                obj_download_end_time = time.time()
+                # calc latency
+                download_obj_time = obj_download_end_time - obj_download_start_time
+                self._bolt_op_times.append(download_obj_time)
+                # count object
+                if blob.content_encoding == "gzip" or str(key).endswith('.gz'):
+                    self._bolt_cmp_obj_count += 1
+                else:
+                    self._bolt_uncmp_obj_count += 1
+                # get blob size
+                self._bolt_obj_sizes.append(blob.size)
 
         # calc bolt perf stats
         bolt_dwnld_obj_pt_perf_stats = self._compute_perf_stats(self._bolt_op_times, obj_sizes=self._bolt_obj_sizes)
@@ -241,11 +253,15 @@ class BoltGSPerf:
         else:
             bolt_dwnld_obj_pt_stat_name = 'bolt_download_obj_pt_perf_stats'
 
-        return {
+        download_obj_pt_perf_stats = {
             bolt_dwnld_obj_pt_stat_name: bolt_dwnld_obj_pt_perf_stats,
             'bolt_object_count (compressed)': self._bolt_cmp_obj_count,
             'bolt_object_count (uncompressed)': self._bolt_uncmp_obj_count
         }
+        if self._request_type == "ALL":
+            return download_obj_pt_perf_stats
+        else:
+            return json.dumps(download_obj_pt_perf_stats, indent=4, sort_keys=True)
 
     def _upload_object_perf(self, bucket_name):
         # Upload objects to Bolt/GS.
@@ -278,11 +294,15 @@ class BoltGSPerf:
         # calc bolt perf stats
         bolt_upload_obj_perf_stats = self._compute_perf_stats(self._bolt_op_times)
 
-        return {
+        upload_obj_perf_stats = {
             'object_size': "{:d} bytes".format(self.OBJ_LENGTH),
             'gs_upload_obj_perf_stats': gs_upload_obj_perf_stats,
             'bolt_upload_obj_perf_stats': bolt_upload_obj_perf_stats
         }
+        if self._request_type == "ALL":
+            return upload_obj_perf_stats
+        else:
+            return json.dumps(upload_obj_perf_stats, indent=4, sort_keys=True)
 
     def _delete_object_perf(self, bucket_name):
         # Delete Objects from Bolt/GS.
@@ -313,10 +333,14 @@ class BoltGSPerf:
         # calc bolt perf stats
         bolt_del_obj_perf_stats = self._compute_perf_stats(self._bolt_op_times)
 
-        return {
+        del_obj_perf_stats = {
             'gs_del_obj_perf_stats': gs_del_obj_perf_stats,
             'bolt_del_obj_perf_stats': bolt_del_obj_perf_stats
         }
+        if self._request_type == "ALL":
+            return del_obj_perf_stats
+        else:
+            return json.dumps(del_obj_perf_stats, indent=4, sort_keys=True)
 
     def _all_perf(self, bucket_name):
         # Upload / Delete Objects using generated key names.
@@ -333,10 +357,11 @@ class BoltGSPerf:
         self._keys = self._list_objects(bucket_name)
         download_obj_perf_stats = self._download_object_perf(bucket_name)
 
-        return self._merge_perf_stats(upload_obj_perf_stats,
-                                      download_obj_perf_stats,
-                                      del_obj_perf_stats,
-                                      list_objs_perf_stats)
+        all_perf_stats = self._merge_perf_stats(upload_obj_perf_stats,
+                                                download_obj_perf_stats,
+                                                del_obj_perf_stats,
+                                                list_objs_perf_stats)
+        return json.dumps(all_perf_stats, indent=4, sort_keys=True)
 
     def _merge_perf_stats(self, *perf_stats):
         """
